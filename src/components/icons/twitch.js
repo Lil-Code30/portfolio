7c1fa76cd7320b66730bd88e558e1b623edbe402
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-07-13 07:53:57 -0400

    feat: Add Twitch and YouTube icons
    Exports new Twitch and YouTube SVG icons and wires them into the icon switch so they can be rendered by name.

---
+++ src/components/icons/twitch.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const IconTwitch = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className="feather feather-twitch">
+    <title>Twitch</title>
+    <path d="M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7"></path>
+  </svg>
+);
+
+export default IconTwitch;
